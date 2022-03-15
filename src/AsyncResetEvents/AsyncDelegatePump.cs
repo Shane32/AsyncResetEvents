758ac2f8087a4d6c625a7f836c57aa4f47cb76d8
Shane32 <shane@acdmail.com>
2022-03-15 17:56:31 -0400

    Update formatting

--- src/AsyncResetEvents/AsyncDelegatePump.cs
+++ src/AsyncResetEvents/AsyncDelegatePump.cs
@@ -1,80 +1,77 @@
-#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+namespace Shane32.AsyncResetEvents;
 
-namespace Shane32.AsyncResetEvents
+/// <summary>
+/// An asynchronous delegate pump, where queued asynchronous delegates are
+/// executed in order.
+/// </summary>
+public class AsyncDelegatePump : AsyncMessagePump<Func<Task>>
 {
     /// <summary>
-    /// An asynchronous delegate pump, where queued asynchronous delegates are
-    /// executed in order.
+    /// Initializes a new instance.
     /// </summary>
-    public class AsyncDelegatePump : AsyncMessagePump<Func<Task>>
+    public AsyncDelegatePump() : base(d => d())
     {
-        /// <summary>
-        /// Initializes a new instance.
-        /// </summary>
-        public AsyncDelegatePump() : base(d => d())
-        {
-        }
+    }
 
-        /// <summary>
-        /// Executes a delegate in order and returns its result.
-        /// </summary>
-        public Task SendAsync(Func<Task> action)
-        {
+    /// <summary>
+    /// Executes a delegate in order and returns its result.
+    /// </summary>
+    public Task SendAsync(Func<Task> action)
+    {
 #if NET5_0_OR_GREATER
-            var tcs = new TaskCompletionSource();
+        var tcs = new TaskCompletionSource();
 #else
-            var tcs = new TaskCompletionSource<byte>();
+        var tcs = new TaskCompletionSource<byte>();
 #endif
-            Post(() => {
-                Task task;
-                try {
-                    task = action();
-                } catch (Exception ex) {
-                    tcs.SetException(ex);
-                    throw;
-                }
-                task.ContinueWith(task2 => {
-                    if (task2.IsFaulted)
-                        tcs.SetException(task2.Exception!.GetBaseException());
-                    else if (task2.IsCanceled)
-                        tcs.SetCanceled();
-                    else
+        Post(() => {
+            Task task;
+            try {
+                task = action();
+            } catch (Exception ex) {
+                tcs.SetException(ex);
+                throw;
+            }
+            task.ContinueWith(task2 => {
+                if (task2.IsFaulted)
+                    tcs.SetException(task2.Exception!.GetBaseException());
+                else if (task2.IsCanceled)
+                    tcs.SetCanceled();
+                else
 #if NET5_0_OR_GREATER
-                        tcs.SetResult();
+                    tcs.SetResult();
 #else
-                        tcs.SetResult(0);
+                    tcs.SetResult(0);
 #endif
-                }, TaskContinuationOptions.ExecuteSynchronously);
-                return task;
-            });
-            return tcs.Task;
-        }
+            }, TaskContinuationOptions.ExecuteSynchronously);
+            return task;
+        });
+        return tcs.Task;
+    }
 
-        /// <summary>
-        /// Executes a delegate in order and returns its result.
-        /// </summary>
-        public Task<T> SendAsync<T>(Func<Task<T>> action)
-        {
-            var tcs = new TaskCompletionSource<T>();
-            Post(() => {
-                Task<T> task;
-                try {
-                    task = action();
-                } catch (Exception ex) {
-                    tcs.SetException(ex);
-                    throw;
-                }
-                task.ContinueWith(task2 => {
-                    if (task2.IsFaulted)
-                        tcs.SetException(task2.Exception!.GetBaseException());
-                    else if (task2.IsCanceled)
-                        tcs.SetCanceled();
-                    else
-                        tcs.SetResult(task2.Result);
-                }, TaskContinuationOptions.ExecuteSynchronously);
-                return task;
-            });
-            return tcs.Task;
-        }
+    /// <summary>
+    /// Executes a delegate in order and returns its result.
+    /// </summary>
+    public Task<T> SendAsync<T>(Func<Task<T>> action)
+    {
+        var tcs = new TaskCompletionSource<T>();
+        Post(() => {
+            Task<T> task;
+            try {
+                task = action();
+            } catch (Exception ex) {
+                tcs.SetException(ex);
+                throw;
+            }
+            task.ContinueWith(task2 => {
+                if (task2.IsFaulted)
+                    tcs.SetException(task2.Exception!.GetBaseException());
+                else if (task2.IsCanceled)
+                    tcs.SetCanceled();
+                else
+                    tcs.SetResult(task2.Result);
+            }, TaskContinuationOptions.ExecuteSynchronously);
+            return task;
+        });
+        return tcs.Task;
     }
 }
